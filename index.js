c16fdb9dd194fa33bd7b9e7ab3f68d80409a8af2
Jane Kim <jane@janekim.me>
2018-07-30 20:10:26 -0400

    simple queen lookup

--- index.js
+++ index.js
@@ -1,52 +1,40 @@
 const { ApolloServer, gql } = require('apollo-server')
 
-// This is a (sample) collection of books we'll be able to query
-// the GraphQL server for.  A more complete example might fetch
-// from an existing data source like a REST API or database.
-const books = [
+const queens = [
   {
-    title: 'Harry Potter and the Chamber of Secrets',
-    author: 'J.K. Rowling'
+    name: 'Aquaria',
+    appearances: ['Season 10']
   },
   {
-    title: 'Jurassic Park',
-    author: 'Michael Crichton'
+    name: 'Shangela',
+    appearances: ['Season 2', 'Season 3', 'All Stars Season 3']
+  },
+  {
+    name: 'RuPaul',
+    nickname: 'Mama Ru'
   }
 ]
 
-// Type definitions define the "shape" of your data and specify
-// which ways the data can be fetched from the GraphQL server.
 const typeDefs = gql`
-  # Comments in GraphQL are defined with the hash (#) symbol.
-
-  # This "Book" type can be used in other type declarations.
-  type Book {
-    title: String
-    author: String
+  type Queen {
+    name: String!
+    nickname: String
+    appearances: [String]
   }
 
-  # The "Query" type is the root of all GraphQL queries.
-  # (A "Mutation" type will be covered later on.)
   type Query {
-    books: [Book]
+    queens: [Queen]
   }
 `
 
-// Resolvers define the technique for fetching the types in the
-// schema.  We'll retrieve books from the "books" array above.
 const resolvers = {
   Query: {
-    books: () => books
+    queens: () => queens
   }
 }
 
-// In the most basic sense, the ApolloServer can be started
-// by passing type definitions (typeDefs) and the resolvers
-// responsible for fetching the data for those types.
 const server = new ApolloServer({ typeDefs, resolvers })
 
-// This `listen` method launches a web-server.  Existing apps
-// can utilize middleware options, which we'll discuss later.
 server.listen().then(({ url }) => {
   console.log(`🚀  Server ready at ${url}`)
 })
